027fb1c8463c32e4c9f460ee60fd6e8722f8190f
ma2snct <knightmarespark@gmail.com>
2015-10-14 15:13:48 +0900

    getとlistの違いを痛感

--- routes/add.js
+++ routes/add.js
@@ -3,7 +3,7 @@ var router = express.Router();
 //追加モジュール
 var nano = require('nano')('http://localhost:5984');
 var db = require('nano')('http://localhost:5984/mtg_score');
-var sotsu = nano.use('mtg_score');
+var mtg_score = nano.use('mtg_score');
 var assert = require('assert');
 
 var rows = [];
@@ -14,11 +14,12 @@ router.get('/', function(req, res, next) {
 	y = now.getFullYear();
 	m = now.getMonth()+1;
 	d = now.getDate();
-	db.insert({year:y, month:m, date:d}, 'ブードラ2', function(err, body, header){
+	mtg_score.insert({year:y, month:m, date:d}, 'ブードラ2', function(err, body, header){
 		if(err){
 			console.log('it has err');
 		}
 	});
+	res.render('index');
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var nano = require('nano')('http://localhost:5984');
+var db = require('nano')('http://localhost:5984/mtg_score');
+var mtg_score = nano.use('mtg_score');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  mtg_score.list(function(err, body){
+    if(err){
+      console.log('cannot get list');
+    }
+    res.render('index', { title: 'MTG-SCORE', rows:body.rows });
+    console.log(body.rows)
+  });
+
+  mtg_score.get('ブードラ', function(err, body){
+    console.log(body);
+  });
 });
 
 module.exports = router;
